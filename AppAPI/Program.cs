f5da77ba5ccef6b39aa6765f92b67e56726288a8
Jim Vacca <jamesvac@microsoft.com>
2020-09-07 23:21:20 +0000

    add logic for cert processing

--- AppAPI/Program.cs
+++ AppAPI/Program.cs
@@ -6,25 +6,49 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 namespace AppAPI
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+             var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddUserSecrets<Program>();
+
+                EarlyConfiguration = config.Build();               
+            
+             CreateHostBuilder(args)
+                .Build()
+                .Run();
+            
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IConfigurationRoot EarlyConfiguration { get; set; }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        { 
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    
-                    webBuilder.UseStartup<Startup>();
+                    {     
+                        string DevEnvironmentCheck = EarlyConfiguration["ASPNETCORE_ENVIRONMENT"];
+                        string CertPassword = "";
+                        if (DevEnvironmentCheck.Equals("Development"))
+                            {
+                                CertPassword = EarlyConfiguration["Kestrel:Certificates:Development:Password"];                   
+                                webBuilder.UseKestrel(opts =>
+                                    {  opts.ListenLocalhost(5001, opts => opts.UseHttps("localhost.pfx",CertPassword)); 
+                                        });
+                            };
+                        
+                        webBuilder.UseStartup<Startup>();       
+                    });
+        }       
 
-                    // ClientCertificateMode.RequireCertificate
-
-                });
-    }
-}
+        
+    } // end program class
+} // end namespace
